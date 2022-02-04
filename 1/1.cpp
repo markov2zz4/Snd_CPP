19184410f5a3ff270e31db7fdf64efbc3b5ec90f
MarkoW <albertnov@internet.ru>
2022-02-05 00:37:53 +0700

    1 added

---
+++ 1/1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void printTime(const int h, const int m, const int s) {
+    printf("%02d:%02d:%02d\n", h, m, s);
+
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int h, m, s;
+
+    for (int i{ 0 }; i < N; ++i) {
+        cin >> h >> m >> s;
+
+        if (h < 0 || h > 23)
+            throw "Invalid Argument";
+        if (m < 0 || m > 59)
+            throw "Invalid Argument";
+        if (s < 0 || s > 59)
+            throw "Invalid Argument";
+
+        printTime(h, m, s);
+    }
+    return 0;
+}
+
+
