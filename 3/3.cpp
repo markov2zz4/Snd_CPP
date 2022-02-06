9201994875664536a97a9efe672b2bb7a2258298
Lksih <odindwatri123123@gmail.com>
2022-02-06 15:33:01 +0700

    Changed 3 to counting seconds

--- 3/3.cpp
+++ 3/3.cpp
@@ -8,6 +8,25 @@ struct DateTime_s {
     int hours, minutes, seconds;
 };
 
+unsigned long long int CountSeconds(DateTime_s* ptrAdr) {
+    int DaysInMonth[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
+    int DaysQuan{ 365 };
+    unsigned long long Result{ 0 };
+    if (ptrAdr->year % 4 == 0) {
+        DaysInMonth[1]++;
+        DaysQuan++;
+    }
+    Result = ptrAdr->year * DaysQuan;
+    for (int i{ 0 }; i < ptrAdr->month - 1; i++) {
+        Result += DaysInMonth[i];
+    }
+    Result += ptrAdr->day;
+    Result = Result * 24 + ptrAdr->hours;
+    Result = Result * 60 + ptrAdr->minutes;
+    Result = Result * 60 + ptrAdr->seconds;
+    return Result;
+}
+
 void CheckRight(const int year, const int month, const int day, const int hours, const int minutes, const int seconds) {
     if ((year < 1 || year > 5000) || (month < 1 || month > 12) || (day < 1 || day > 31))
         throw "Invalid argument";
@@ -26,26 +45,11 @@ void DateTimeMin(DateTime_s** ptrAdr, const int rows) {
     {
         //DateTime_s* min = ptrAddresses[i];
 
-        if (ptrAdr[i]->year <= minY)
-            minY = ptrAdr[i]->year;
+        if (CountSeconds(ptrAdr[i]) <= CountSeconds(ptrAdr[index])) {
+            index = i;
+        }
 
-        if (ptrAdr[i]->month <= minMonth)
-            minMonth = ptrAdr[i]->month;
-
-        if (ptrAdr[i]->day <= minD)
-            minD = ptrAdr[i]->day;
-
-        if (ptrAdr[i]->hours <= minH)
-            minH = ptrAdr[i]->hours;
-
-        if (ptrAdr[i]->minutes <= minMinutes)
-            minMinutes = ptrAdr[i]->minutes;
-
-        else if (ptrAdr[i]->seconds <= minS)
-            minS = ptrAdr[i]->seconds;
-        index = i;
-        
-    }  
+    }
 
     cout << ptrAdr[index]->year << " " << ptrAdr[index]->month << " " << ptrAdr[index]->day << " ";
     cout << ptrAdr[index]->hours << " " << ptrAdr[index]->minutes << " " << ptrAdr[index]->seconds << endl;
@@ -55,17 +59,17 @@ void DateTimeMin(DateTime_s** ptrAdr, const int rows) {
 DateTime_s** CreateArrayStruct(const int rows) {
 
     DateTime_s* ptrDTime = new DateTime_s[rows];
-    DateTime_s** ptrAddresses = new DateTime_s*[rows];
+    DateTime_s** ptrAddresses = new DateTime_s * [rows];
 
     for (int i = 0; i < rows; i++) {
         cin >> ptrDTime[i].year >> ptrDTime[i].month >> ptrDTime[i].day >> ptrDTime[i].hours >> ptrDTime[i].minutes >> ptrDTime[i].seconds;
-        CheckRight(ptrDTime[i].year, ptrDTime[i].month, ptrDTime[i].day ,ptrDTime[i].hours, ptrDTime[i].minutes, ptrDTime[i].seconds);
+        CheckRight(ptrDTime[i].year, ptrDTime[i].month, ptrDTime[i].day, ptrDTime[i].hours, ptrDTime[i].minutes, ptrDTime[i].seconds);
     }
-    
+
     for (int j = 0; j < rows; j++)
         ptrAddresses[j] = &ptrDTime[j];
     return ptrAddresses;
-    
+
 }
 
 int main()
@@ -79,6 +83,5 @@ int main()
 
     DateTime_s** ptrAddresses = CreateArrayStruct(rows);
     DateTimeMin(ptrAddresses, rows);
-    
-}
 
+}
