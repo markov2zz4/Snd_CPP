c314b6cf714ffe4e3ff51267f8c5a9f57da699ca
MarkoW <albertnov@internet.ru>
2022-02-09 18:09:24 +0700

    Added 4 successfully

--- 4/4.cpp
+++ 4/4.cpp
@@ -6,46 +6,78 @@ using namespace std;
 
 struct Label_s {
 
-	string name;
+	char name[16];
 	int age = 0;
 
 };
-
 struct NameStats_s {
 
 	int cntTotal, cntLong;
 
 };
-
 struct AgeStats_s {
 
 	int cntTotal, cntAdults, cntKids;
 
 };
 
-char* slice(char* s, int from, int to)
-{
-	memmove(s, s + from, to - from + 1);
-	s[to - from + 1] = 0;
-	return s;
-};
+void CheckRight(const char* name, const int age) {
 
-void calcStats() {
+	for (int i{ 0 }; i < strlen(name); i++)
+		if (isdigit(name[i]))
+			throw "Invalid: an argument that is not valid.";
 
-	
+	if (strlen(name) < 2 || strlen(name) > 15)
+		throw "Invalid: an argument that is not valid.";
+	if (age < 0 || age > 5000)
+		throw "Invalid: an argument that is not valid.";
+}
 
-	//stringstream strField(str);
-	//cin >> str;
+Label_s* CreateStruct(const int fields) {
 
-	//str.erase(0, str.find(' ') + 1);
+	Label_s* oLabel = new Label_s[fields];
 
-		/*for (int i = 0; i < fields; i++)
-		{
-			cin >> str;
-			str.erase(0, str.find('let') + 1);
-			stringstream(str) >> ptrLabel[i].name;
-		}
-		*/
+	char* waste = new char[fields * 3];
+
+	for (int i = 0; i < fields; i++) {
+		cin >> oLabel[i].name >> oLabel[i].age >> waste;
+		char* pName = oLabel[i].name;
+		CheckRight(pName, oLabel[i].age);
+	}
+	return oLabel;
+}
+
+void calcStats(Label_s* oLabel, NameStats_s* oNameStats, AgeStats_s* oAgeStats, const int fields) {
+
+
+	int cntTotalAge{0}, cntAdults{ 0 }, cntKids{ 0 };
+	int cntTotalName{0}, cntLong{ 0 };
+
+	for (int i{ 0 }; i < fields; i++) {
+
+		if (strlen(oLabel[i].name) > 10) cntLong++;
+
+		if (oLabel[i].age >= 18) cntAdults++;
+		else if (oLabel[i].age < 14) cntKids++;
+
+		cntTotalAge++; cntTotalName++;
+	}
+
+	oNameStats->cntLong = cntLong;
+	oNameStats->cntTotal = cntTotalName;
+
+	oAgeStats->cntAdults = cntAdults;
+	oAgeStats->cntKids = cntKids;
+	oAgeStats->cntTotal = cntTotalAge;
+}
+
+void PrintStruct(NameStats_s* oNameStats, AgeStats_s* oAgeStats) {
+	cout << "names: total = " << oNameStats->cntTotal << endl;
+	cout << "names: long = " << oNameStats->cntLong << endl;
+
+	cout << "ages: total = " << oAgeStats->cntTotal << endl;
+	cout << "ages: adult = " << oAgeStats->cntAdults << endl;
+	cout << "ages: kid = " << oAgeStats->cntKids << endl;
 
 }
 
@@ -55,30 +87,15 @@ int main() {
 	int fields;
 	cin >> fields;
 
-	Label_s* oLabel = new Label_s[fields];
-	//DateTime_s** ptrAddresses = new DateTime_s * [rows];
+	if (fields <= 1 || fields > 1000)
+		throw "Invalid: an argument that is not valid.";
 
-#if 0
-	for (int i = 0; i < fields; i++)
-	{
+	Label_s* oLabel{ CreateStruct(fields) };
+	NameStats_s* oNameStats = new NameStats_s;
+	AgeStats_s* oAgeStats = new AgeStats_s;
 
-		cin >> name;
-		name.erase(0, name.find('let')+1);
-		cout << name;
-
-		str = name;
-
-		oLabel[i].name = name.erase(0, name.find(' ') + 1);
-		
-
-		str.erase(0, str.find('let') + 1);
-		ageStream.str(str);
-		ageStream >> age;
-		cout << "age = " << age << endl;
-		oLabel[i].age = age;
-		
-#endif	
-		//cout << "name is " << name << endl << "age is "<< age << endl;
+	calcStats(oLabel, oNameStats, oAgeStats, fields);
+	PrintStruct(oNameStats, oAgeStats);
 	}
 	
 	
