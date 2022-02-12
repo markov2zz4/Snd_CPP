66affac7190bcffd0851dee28732b77050ac0762
MarkoW <albertnov@internet.ru>
2022-02-12 23:06:21 +0700

    5

--- 6/6.cpp
+++ 6/6.cpp
@@ -1,23 +1,63 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream>
 #include <stdexcept>
+
 using namespace std;
 
 void reverse(char* start, const int len) {
-
+	for (int i = strlen(start); i >= 0; i--)
+		cout << start[i];
+	cout << endl;
 }
 
 
 int main()
 {
-    int CountString;
-    cin >> CountString;
 
-    if (CountString < 2 || CountString > 1000)
-        throw("Out of range");
+	ofstream file;
+	ifstream iFile("input.txt");
 
+	int fields;
 
+	const int size = 100 + 1;
+	char line[size];
 
+	char oLines[size];
+	int len = 0;
+		
+	cin >> fields;
 
+	if (fields < 2 || fields > 1000)
+		throw invalid_argument("Invalid argument");
+
+	file.open("input.txt");
+	for (int i = 0; i < fields; i++)
+	{
+		cin >> line;
+
+		for (int k = 0; k < strlen(line); k++)
+		{
+			if ((line[k] < 'a' && line[k] > 'z') && (line[k] < 'A' && line[k] > 'Z'))
+				throw invalid_argument("Invalid argument");
+		}
+
+		file << line << "\n";
+	}
+	file.close();
+
+	for (int k = 0; k < fields; k++)
+	{
+		len = 0;
+		iFile.getline(oLines, size);
+		len = strlen(oLines);
+
+		reverse(oLines, len);
+		
+	}
+	
+
+	iFile.close();
 
 }
 
