51838f7ed2d92990bba45ae2c9f6b62d59560673
MarkoW <81167144+markov2zz4@users.noreply.github.com>
2022-02-16 21:15:33 +0300

    Add files via upload

---
+++ 8/8.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+char* concat(char* pref, char* suff) {
+    strcat(pref, suff);
+    return pref;
+}
+
+int main()
+{
+    int fields{ 0 };
+    std::cin >> fields;
+
+    if (fields < 2 || fields > 10000)
+        return -1;
+
+    char* pref{ new char[100 + 1]{""}};
+    
+
+    for (int i = 0; i < fields; i++)
+    {
+        char* suff{ new char[100 + 1] };
+        std::cin >> suff;
+        
+
+        for (int k = 0; k < strlen(suff); k++)
+        {
+            if (((int)suff[k] < 'a' || (int)suff[k] > 'z') && ((int)suff[k] < 'A' || (int)suff[k] > 'Z'))
+                return -1;
+
+        }
+        pref = concat(pref, suff);
+    }
+
+    
+    cout << pref << endl;
+}
