4fac6eccf59c5420b7749129389fc02d695220bf
MarkoW <albertnov@internet.ru>
2022-03-24 00:27:10 +0700

    7

---
+++ 7/7.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int SIZE = 255;
+
+typedef struct Tokens_s {
+
+	int num;
+	char** str;
+
+} Tokens;
+
+void printStr(char* ptrStr, const int size) {
+	for (int i{ 0 }; i < size; ++i)
+		cout << ptrStr[i];
+	cout << endl;
+}
+
+int main() {
+
+	char* str = new char[SIZE] {""};
+
+	printStr(str, SIZE);
+
+}
